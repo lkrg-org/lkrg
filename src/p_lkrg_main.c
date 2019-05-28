50f76ee9ed638cec578489e183bbb3b5e01ebd3c
Adam_pi3 <pi3@pi3.com.pl>
2019-05-28 01:36:26 +0000

    Minor change of initialization logic

--- src/p_lkrg_main.c
+++ src/p_lkrg_main.c
@@ -22,6 +22,9 @@ unsigned int p_init_log_level = 1;
 static enum cpuhp_state p_hot_cpus;
 #endif
 
+int (*p_freeze_processes)(void) = 0x0;
+void (*p_thaw_processes)(void) = 0x0;
+
 /*
  * Main entry point for the module - initialization.
  */
@@ -59,6 +62,28 @@ static int __init p_lkrg_register(void) {
                "kallsyms_lookup_name() => 0x%lx\n",(long)p_kallsyms_lookup_name);
      }
 #endif
+
+   p_freeze_processes = (int (*)(void))p_kallsyms_lookup_name("freeze_processes");
+
+   if (!p_freeze_processes) {
+      p_print_log(P_LKRG_ERR,
+             "ERROR: Can't find 'freeze_processes' function :( Exiting...\n");
+      p_ret = P_LKRG_GENERAL_ERROR;
+      goto p_main_error;
+   }
+
+   p_thaw_processes = (void (*)(void))p_kallsyms_lookup_name("thaw_processes");
+
+   if (!p_thaw_processes) {
+      p_print_log(P_LKRG_ERR,
+             "ERROR: Can't find 'thaw_processes' function :( Exiting...\n");
+      p_ret = P_LKRG_GENERAL_ERROR;
+      goto p_main_error;
+   }
+
+   // Freeze all non-kernel processes
+   while (p_freeze_processes())
+      schedule();
 
    /*
     * First, we need to plant *kprobes... Before DB is created!
@@ -143,33 +168,38 @@ static int __init p_lkrg_register(void) {
    p_print_log(P_LKRG_CRIT,
           "LKRG initialized successfully!\n");
 
-   return P_LKRG_SUCCESS;
+   p_ret = P_LKRG_SUCCESS;
 
 p_main_error:
 
+   if (p_ret != P_LKRG_SUCCESS) {
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4,10,0)
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,15,0)
-   if (p_cpu)
-      unregister_hotcpu_notifier(&p_cpu_notifier);
+      if (p_cpu)
+         unregister_hotcpu_notifier(&p_cpu_notifier);
 #else
-   if (p_cpu) {
-      cpu_notifier_register_begin();
-      __unregister_hotcpu_notifier(&p_cpu_notifier);
-      cpu_notifier_register_done();
-   }
+      if (p_cpu) {
+         cpu_notifier_register_begin();
+         __unregister_hotcpu_notifier(&p_cpu_notifier);
+         cpu_notifier_register_done();
+      }
 #endif
 #else
-   if (p_cpu)
-      cpuhp_remove_state_nocalls(p_hot_cpus);
+      if (p_cpu)
+         cpuhp_remove_state_nocalls(p_hot_cpus);
 #endif
 
-   p_exploit_detection_exit();
-   p_deregister_module_notifier();
-   p_offload_cache_delete();
-   if (p_db.p_CPU_metadata_array) {
-      kzfree(p_db.p_CPU_metadata_array);
-      p_db.p_CPU_metadata_array = NULL;
+      p_exploit_detection_exit();
+      p_deregister_module_notifier();
+      p_offload_cache_delete();
+      if (p_db.p_CPU_metadata_array) {
+         kzfree(p_db.p_CPU_metadata_array);
+         p_db.p_CPU_metadata_array = NULL;
+      }
    }
+
+   // Thaw all non-kernel processes
+   p_thaw_processes();
 
    return p_ret;
 }
@@ -185,6 +215,10 @@ static void __exit p_lkrg_deregister(void) {
    p_print_log(P_LKRG_DBG,
           "I should never be here! This operation probably is going to break your system! Goodbye ;)\n");
 #endif
+
+   // Freeze all non-kernel processes
+   while (p_freeze_processes())
+      schedule();
 
    del_timer(&p_timer);
    p_deregister_notifiers();
@@ -210,6 +244,9 @@ static void __exit p_lkrg_deregister(void) {
 
    if (p_db.p_CPU_metadata_array)
       kzfree(p_db.p_CPU_metadata_array);
+
+   // Thaw all non-kernel processes
+   p_thaw_processes();
 
    p_print_log(P_LKRG_CRIT, "LKRG unloaded!\n");
 }
