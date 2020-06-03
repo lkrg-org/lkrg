d57b4c0f0e63d4d88761e098c53280967f2d1aec
Adam_pi3 <pi3@pi3.com.pl>
2020-06-03 16:28:30 -0400

    Since kernel 5.7 "kallsyms_lookup_name" and "kallsyms_on_each_symbol" functions are not exported. This patch (hack) retrives necessary function pointer

--- src/modules/ksyms/p_resolve_ksym.c
+++ src/modules/ksyms/p_resolve_ksym.c
@@ -21,6 +21,14 @@
 //unsigned long (*p_kallsyms_lookup_name)(const char *name) = 0x0;
 
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,7,0))
+
+static int p_tmp_kprobe_handler(struct kprobe *p_ri, struct pt_regs *p_regs) {
+   return 0x0;
+}
+
+#else
+
 static int p_lookup_syms_hack(void *unused, const char *name,
                               struct module *mod, unsigned long addr) {
 
@@ -32,14 +40,46 @@ static int p_lookup_syms_hack(void *unused, const char *name,
    return 0x0;
 }
 
+#endif
+
 long get_kallsyms_address(void) {
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,7,0))
+   struct kprobe p_kprobe;
+#else
    int p_tmp = 0x0;
+#endif
    int p_ret = P_LKRG_SUCCESS;
 
 // STRONG_DEBUG
    p_debug_log(P_LKRG_STRONG_DBG,
           "Entering function <get_kallsyms_address>\n");
+
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,7,0))
+
+   /* "Inspiried" idea from LTTng module */
+   memset(&p_kprobe, 0, sizeof(p_kprobe));
+   p_kprobe.pre_handler = p_tmp_kprobe_handler;
+   p_kprobe.symbol_name = "kallsyms_lookup_name";
+   if ( (p_ret = register_kprobe(&p_kprobe)) < 0) {
+      p_print_log(P_LKRG_ERR,
+             "[get_kallsyms_address] register_kprobe error [%d] :(\n",p_ret);
+      p_ret = P_LKRG_GENERAL_ERROR;
+      goto get_kallsyms_address_out;
+   }
+   P_SYM(p_kallsyms_lookup_name) =
+         (unsigned long (*)(const char*))((unsigned long)p_kprobe.addr);
+
+#ifdef CONFIG_ARM
+#ifdef CONFIG_THUMB2_KERNEL
+   if (P_SYM(p_kallsyms_lookup_name))
+      P_SYM(p_kallsyms_lookup_name) |= 1; /* set bit 0 in address for thumb mode */
+#endif
+#endif
+
+   unregister_kprobe(&p_kprobe);
+
+#else
 
    if ( (p_tmp = kallsyms_on_each_symbol(p_lookup_syms_hack,NULL)) == 0x0) {
 // DEBUG
@@ -54,6 +94,7 @@ long get_kallsyms_address(void) {
           p_tmp,
           (unsigned long)P_SYM(p_kallsyms_lookup_name));
 
+#endif
 
 get_kallsyms_address_out:
 
