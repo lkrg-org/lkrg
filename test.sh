8e3abdb7a8d003858581e49b9de20bc4e42f0e6f
Solar Designer <solar@openwall.com>
2021-04-12 14:09:33 +0200

    test.sh: Add comment explaining the script is for out-of-tree

--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
 #!/bin/sh -eux
+# This script is used by out-of-tree (a tool to assist kernel module testing),
+# https://github.com/jollheef/out-of-tree
 dmesg | grep 'LKRG initialized successfully!'
