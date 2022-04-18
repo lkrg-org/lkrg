eb8fbebbf00e1f6207785426d69165f78bdf5691
Vitaly Chikunov <vt@altlinux.org>
2022-04-19 01:02:20 +0300

    CI: Run boot-tests script with UMH test
    Trigger loading of `vhost_vsock` module by the kernel, which uses UMH
    to call `modprobe`.
    
    As a side effect copy source into /lkrg of mkosi boot image.
    
    Signed-off-by: Vitaly Chikunov <vt@altlinux.org>

---
+++ .github/workflows/run-boot-tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Run some tests
+#
+# Copyright (c) 2022 Vitaly Chikunov <vt@altlinux.org>.
+#
+
+set -eux -o pipefail
+
+# Trigger loading vhost_vsock module (using UMH call to modprobe).
+# Device numbers are from /lib/modules/*/modules.devname
+mknod /dev/test c 10 241
+true < /dev/test
+dmesg -T | grep 'Registered.*protocol family' | grep -w -e 'PF_VSOCK' -e '40'
+
+# Sleep (watchdog_thresh*2+1) seconds to let hard and soft lockup detectors to work.
+sleep 21
+
+# Failed tests will not output this line.
+echo "$0 - SUCCESS"
