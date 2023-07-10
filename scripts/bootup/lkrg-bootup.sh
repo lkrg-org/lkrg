9b0cc1f545c9cbcf955f6769431ec3e2e0bee82a
Valentin Obst <valentin.obst@fkie.fraunhofer.de>
2023-07-10 09:30:03 +0200

    make bootup scripts independent of cwd, add usage and root checks

--- scripts/bootup/lkrg-bootup.sh
+++ scripts/bootup/lkrg-bootup.sh
@@ -6,13 +6,26 @@
 #  - Adam 'pi3' Zabrocki (http://pi3.com.pl)
 ##
 
-P_LKRG_SYSTEMD="scripts/bootup/systemd/lkrg-systemd.sh"
-P_LKRG_OPENRC="scripts/bootup/openrc/lkrg-openrc.sh"
+set -eu
+
+P_SCRIPT_DIR="$(dirname "$0")"
+P_LKRG_SYSTEMD="${P_SCRIPT_DIR}/systemd/lkrg-systemd.sh"
+P_LKRG_OPENRC="${P_SCRIPT_DIR}/openrc/lkrg-openrc.sh"
 
 P_RED='\033[0;31m'
 P_GREEN='\033[0;32m'
 P_WHITE='\033[1;37m'
 P_NC='\033[0m' # No Color
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 (install|uninstall)" >&2
+	exit 1
+fi
+
+if [ "$(id -u)" -ne 0 ]; then
+	echo "Please run as root." >&2
+	exit 1
+fi
 
 echo -e " ${P_GREEN}[*] ${P_WHITE}Executing LKRG's bootup installation script${P_NC}"
 
