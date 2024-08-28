2c1b850659caabaa9d7daaa9f52ee4e2836e0dec
Solar Designer <solar@openwall.com>
2024-08-28 17:00:44 +0200

    scripts/add-exports.sh: Do not export __put_seccomp_filter

--- scripts/add-exports.sh
+++ scripts/add-exports.sh
@@ -5,6 +5,4 @@ cd "$KDIR"
 # Add critical exports to prevent them from being optimized-out
 echo "EXPORT_SYMBOL(change_page_attr_set_clr);" >> arch/x86/mm/pat/set_memory.c
 echo "EXPORT_SYMBOL(lookup_fast);" >> fs/namei.c
-# keep symbol export inside the ifdef block defining the symbol
-sed -i '/static void __seccomp_filter_release.*/iEXPORT_SYMBOL(__put_seccomp_filter);\n' kernel/seccomp.c
 echo "EXPORT_SYMBOL(do_seccomp);" >> kernel/seccomp.c
