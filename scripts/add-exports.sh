3cd850e524083ea28bac1e55188564c1d2b47d47
Solar Designer <solar@openwall.com>
2024-09-19 18:06:46 +0200

    scripts/add-exports.sh: Remove the static keyword from needed symbols
    For LKRG, either export or non-static is sufficient.  Adding exports felt
    less invasive and more correct and reliable than dropping static, but
    recent kernels no longer allow (fail on "modpost") to export symbols that
    are static.  Let's do both now.
    
    Fixes #337
    
    Co-authored-by: Alexander Pick <contact@alexander-pick.com>

--- scripts/add-exports.sh
+++ scripts/add-exports.sh
@@ -6,3 +6,7 @@ cd "$KDIR"
 echo "EXPORT_SYMBOL(change_page_attr_set_clr);" >> arch/x86/mm/pat/set_memory.c
 echo "EXPORT_SYMBOL(lookup_fast);" >> fs/namei.c
 echo "EXPORT_SYMBOL(do_seccomp);" >> kernel/seccomp.c
+# Fix modpost errors
+sed -i 's/static int change_page_attr_set_clr/int change_page_attr_set_clr/' arch/x86/mm/pat/set_memory.c
+sed -i 's/static struct dentry \*lookup_fast/struct dentry \*lookup_fast/' fs/namei.c
+sed -i 's/static long do_seccomp/long do_seccomp/' kernel/seccomp.c
