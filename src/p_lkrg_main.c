360bb89ce45af88e3616e5839c62c23acbbefc46
Sultan Alsawaf <sultan@ciq.com>
2026-04-07 21:40:49 -0700

    Fix possible livelock when freezing userspace at init
    When LKRG inits, it tries to freeze all user tasks until success. The
    problem is that some user tasks may be stuck somewhere in the kernel, and
    thus cannot enter the "refrigerator" until they make it back to the
    scheduler. The freezer has no awareness of inter-process dependencies that
    need to be satisfied in order to unblock such tasks, meaning that they
    might depend on an already-frozen task in order to move forward.
    
    When this occurs, the only remedy is to sleep for a bit after the freezing
    attempt to give user tasks some time to run, in the hopes that they will
    become unblocked.
    
    As it stands, there is no such sleep right now: calling schedule() without
    actually changing the current task's state away from TASK_RUNNING either
    does absolutely nothing (on PREEMPT kernels) or puts the current task to
    sleep *if* the current task needs to be scheduled out (like if it's
    exhausted its scheduling quantum).
    
    As a result, a livelock may be observed upon loading LKRG because LKRG will
    keep trying to freeze without giving user tasks a chance to run and satisfy
    the dependencies needed to unblock freezing. The livelock can last for an
    especially long time because the default freeze timeout in the kernel is
    20 seconds, and when there's a blocked task that _can't_ get unblocked
    without something from another task that's _already frozen_, the freeze
    attempt will hopelessly continue until the timeout is reached.
    
    Fix this by adjusting the freeze timeout and implementing an actual sleep
    to allow blocked tasks to become unblocked. The freeze timeout and sleep
    duration are incrementally scaled up to accommodate the needs of different
    systems without possibly livelocking for a long time on systems that can
    freeze quickly.
    
    Fixes: https://github.com/lkrg-org/lkrg/issues/464
    Signed-off-by: Sultan Alsawaf <sultan@ciq.com>

--- src/p_lkrg_main.c
+++ src/p_lkrg_main.c
@@ -424,6 +424,65 @@ static void p_parse_module_params(void) {
 
 }
 
+static void __init p_freeze_userspace(void) {
+
+   unsigned int timeout, tries, orig_timeout, sleep_ms;
+
+   /* Save the original freeze timeout that may have been set by the user */
+   orig_timeout = *P_SYM(p_freeze_timeout_msecs);
+
+   /* Start with a freeze timeout of 500 ms and a sleep duration of 50 ms */
+   timeout = min(500, orig_timeout);
+   sleep_ms = 50;
+
+   /*
+    * Freeze all user tasks with incremental timeouts and sleeps in between. The
+    * reason for starting with a shorter timeout is that a task blocking the
+    * freeze may depend on work from an already-frozen task in order to become
+    * unblocked. In this case, the freeze attempt will always continue until the
+    * timeout is reached, resulting in a long stall where most of userspace is
+    * frozen. The blocking task will always be running somewhere in the kernel,
+    * which is why it is blocking in the first place: it can't just teleport
+    * into the refrigerator when it's already stuck in some other kernel code.
+    *
+    * Some systems may legitimately require more time in order to freeze all
+    * user tasks, which is accommodated by incrementally scaling up the timeout
+    * duration.
+    *
+    * After a failed freeze attempt, we must wait a little bit to allow the
+    * blocking tasks to hopefully make enough forward progress to become
+    * unblocked. There's no precise way to track this, as the freezer is
+    * completely unaware of inter-process dependencies that must be satisfied to
+    * unblock a task.
+    */
+   *P_SYM(p_freeze_timeout_msecs) = timeout;
+   for (tries = 1; P_SYM_CALL(p_freeze_processes); tries++) {
+      /* Scale up after every 3 failed attempts */
+      if (!(tries % 3)) {
+         /* Don't sleep longer than 500 ms at a time (it probably won't help) */
+         sleep_ms = min(sleep_ms + 50, 500);
+
+         /* Increase the timeout, capping it to the original timeout duration */
+         timeout = min(timeout * 2, orig_timeout);
+         *P_SYM(p_freeze_timeout_msecs) = timeout;
+      }
+
+      p_print_log(P_LOG_ISSUE, "Freezing failed %u time(s), sleeping %u ms before next try with timeout of %u ms",
+                  tries, sleep_ms, timeout);
+
+      /* Sleep for a bit to give blocking tasks some time to become unblocked */
+      msleep(sleep_ms);
+   }
+
+   /*
+    * Restore the original freeze timeout. We may overwrite a userspace change
+    * to the freeze timeout via /sys/power/pm_freeze_timeout if the change
+    * occurred between when we cached the original timeout and now. This
+    * shouldn't be a big deal.
+    */
+   *P_SYM(p_freeze_timeout_msecs) = orig_timeout;
+}
+
 /*
  * Main entry point for the module - initialization.
  */
@@ -484,6 +543,7 @@ static int __init p_lkrg_register(void) {
       return P_LKRG_GENERAL_ERROR;
    }
 
+   P_SYM_INIT(freeze_timeout_msecs)
    P_SYM_INIT(freeze_processes)
    P_SYM_INIT(thaw_processes)
 #if defined(CONFIG_X86) && LINUX_VERSION_CODE >= KERNEL_VERSION(5,8,0)
@@ -498,8 +558,7 @@ static int __init p_lkrg_register(void) {
 #endif
 
    // Freeze all non-kernel processes
-   while (P_SYM_CALL(p_freeze_processes))
-      schedule();
+   p_freeze_userspace();
 
    p_freeze = 1;
 
