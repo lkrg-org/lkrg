524a2965cb20acfaab8a3f002ee4d23a97276137
Sultan Alsawaf <sultan@ciq.com>
2026-04-21 22:07:32 -0700

    Make the dummy func noinline in case of LTO
    Prevent LTO from inlining calls to lkrg_dummy() across compilation units.
    
    Signed-off-by: Sultan Alsawaf <sultan@ciq.com>

--- src/modules/integrity_timer/verify_kprobes/p_dummy_func.c
+++ src/modules/integrity_timer/verify_kprobes/p_dummy_func.c
@@ -22,7 +22,7 @@ __attribute__((optnone))
 #else
 __attribute__((optimize(0)))
 #endif
-int lkrg_dummy(int arg) {
+noinline int lkrg_dummy(int arg) {
 
    p_debug_log(P_LOG_DEBUG,
           "[lkrg_dummy] Argument value: [%d]\n",arg);
